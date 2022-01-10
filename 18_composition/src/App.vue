1263cac6ca8cb05b2b11baf2db007fa37f203cfb
lz-kc <li.zhang@kinandcarta.com>
2022-01-10 15:43:44 -0800

    composition api

---
+++ 18_composition/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="container">
+    <h2>{{ user.name }}</h2>
+    <h3>{{ user.age }}</h3>
+    <button @click="setAge">Change Age</button>
+  </section>
+</template>
+
+<script>
+import { reactive } from 'vue';
+export default {
+  // data() {
+  //   return {
+  //     userName: 'Maximilian',
+  //   };
+  // },
+  setup() {
+    const user = reactive({
+      name: 'Li',
+      age: 31,
+    });
+
+    function setNewAge() {
+      user.age += 32;
+    }
+
+    return { user: user, setAge: setNewAge };
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  text-align: center;
+}
+</style>
